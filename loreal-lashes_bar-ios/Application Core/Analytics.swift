031590cefcaf86f18226b5bdfd4f19e9801c2d45
Krzystzof.Dabrowski <krzysztof.dabrowski@mubaloo.com>
2016-11-14 15:20:24 +0000

    Client profiles added

--- loreal-lashes_bar-ios/Application Core/Analytics.swift
+++ loreal-lashes_bar-ios/Application Core/Analytics.swift
@@ -15,14 +15,6 @@ struct Analytics {
     static let context = GGLContext.sharedInstance()
     static let tracker = GAI.sharedInstance().tracker(withTrackingId: trackingID)
     
-    enum Screen: String {
-        case BuildIt = "Build It"
-        case FeaturedPalettes = "Featured Palettes"
-    }
-    
-    enum TimingName: String {
-        case SessionDuration = "Session Duration"
-    }
     
     enum Action: String {
         case ProductDetail = "Lashes Product Detail"
@@ -32,20 +24,12 @@ struct Analytics {
         case AddToPlaylist = "Add to Lashes playlist"
     }
     
-//    enum Label: String {
-//        case Technique = "Technique"
-//        case Brushes = "Brushes"
-//    }
     
     enum Category: String {
         case Technique = "Lashes Technique"
         case Lashes = "Lashes"
         case Email = "Lashes Email"
         case VideoPlayback = "Lashes Video playback Started"
-    }
-    
-    enum Value: Int {
-        case BrushBar = 0
     }
     
     static func initialise() {
@@ -58,28 +42,6 @@ struct Analytics {
     static func dispatch() {
         GAI.sharedInstance().dispatch()
     }
-    
-    static func trackTime(time: TimeInterval, category: Category, name: TimingName? = nil, label: String? = nil) {
-        let parameters = GAIDictionaryBuilder.createTiming(
-            withCategory: category.rawValue,
-            interval: (time * 1000) as TimeInterval as NSNumber!,
-            name: name?.rawValue,
-            label: label
-        ).build() as [NSObject : AnyObject]
-        
-        tracker?.send(parameters)
-    }
-    
-    static func trackScreen(screen: Screen) {
-        tracker?.set(kGAIScreenName, value: screen.rawValue)
-        let data = GAIDictionaryBuilder.createScreenView().build() as [NSObject : AnyObject]
-        tracker?.send(data)
-        
-    }
-    
-    // Use this for tracking catrgory:lushec or brushes 
-    // action from enum like email send or product displayed
-    // parameter = value?
     
     static func sendEvent(category: Category, action: Action, label: String, value: Int = 0) {
         let eventParams = GAIDictionaryBuilder.createEvent(
